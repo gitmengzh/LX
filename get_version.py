28d6c4a0770ae630f587b6323140fe6eea3e9efb
mengzh <mengzh1618@gmail.com>
2018-08-02 00:07:12 +0800

    get file version,but can't get signer

--- get_version.py
+++ get_version.py
@@ -10,17 +10,45 @@
 #1 先编写不带界面工具
 #2 编写带界面工具
 #3 打包成exe
-import win32api
+import win32api,win32com.client
 
 
 def get_file_version(file_path):
 
 
-    version = win32api.GetFileVersionInfo(file_path,'\\')
-    print(version)
+    #info = win32api.GetFileVersionInfo(file_path,'\\')
+    sign = win32com.client.gencache.EnsureDispatch('capicom.signedcode',0)
+    sign.FileName = file_path
+    signer = sign.Signer
+    print(signer.Certificate.IssueName,signer.Certificate.SerialNumber)
+    #ms = info['FileVersionMS']
+    #ls = info['FileVersionLS']
+    #version = '%d.%d.%d.%d'%(win32api.HIWORD(ms),win32api.LOWORD(ms),win32api.HIWORD(ls),win32api.LOWORD(ls))
+    #print(info,version)
 
 
 
 
 
-test = get_file_version("C:\\Windows\\System32\\notepad.exe")
+
+#test = get_file_version("C:\\Windows\\System32\\notepad.exe")
+
+sign = win32com.client.gencache.EnsureDispatch('capicom.signedcode',0)
+sign.FileName = r'C:\Windows\System32\notepad.exe'
+signer = sign.Signer
+print(signer.Certificate.IssueName,signer.Certificate.SerialNumber)
+'''
+GetFileVersionInfo 返回值：
+Signature:
+StrucVersion:
+FileVersionMS:
+FileVersionLS:
+ProductVersionMS:
+ProductVersionLS:
+FileFlagsMask:
+FileFlags:
+FileOS:
+FileType:
+FileSubtype:
+FileDate:
+'''
