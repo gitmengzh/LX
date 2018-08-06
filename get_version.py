8011fbb34cac7190dcf39c0c361ce3f9cbccc000
mengzh <mengzh1618@gmail.com>
2018-08-06 22:56:56 +0800

    can't get sub_folder pe file version

--- get_version.py
+++ get_version.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 
 #编写一个查看CCAV所有文件得签名，版本信息得带界面工具
-#1 先编写不带界面工具
+#1 先编写不带界面工具,完成获取文件版本号工具，接下来其他属性
 #2 编写带界面工具
 #3 打包成exe
 import win32api,win32com.client
@@ -54,52 +54,24 @@ FileSubtype:
 FileDate:
 '''
 
-
-def get_folder_pe_file(folder_path):
-
-    #re.match(r'.exe.dll.sys')
-
-    #file_tpye = ([])
-    names = os.listdir(folder_path)
-    path = []
-    pe_names = []
-    for name in names:
-        path = folder_path+'\\'+name
-        if os.path.isdir(path):
-            path = folder_path+'\\'+name
-        else:
-            if name.endswith(('.exe','.dll','.sys')):
-                pe_names = name
-                print(pe_names)
-                file_path = folder_path+'\\'+pe_names
-
-
-
-
-
-
-#test = get_folder_pe_file("C:\\Program Files (x86)\\COMODO\\COMODO Secure Shopping")
-#test = get_folder_pe_file("C:\\Program Files (x86)\\COMODO\\test")
-
 #通过OS.walk获取文件夹所有指定文件，包括子文件夹中
 def get_all_files(folder_path):
-
+    path2 = {}
+    path = []
+    filenames = []
     for maindir, subdir,file_name_list in os.walk(folder_path):
         for filename in file_name_list:
             if filename.endswith(('.exe','.dll','.sys')):
                 apath = os.path.join(maindir,filename)
-                path = []
-                filenames = []
                 path.append(apath)
                 filenames.append(filename)
 
-                #如何合并两个字典，将字典传给get_version函数
+                #合并两个字典，将字典传给get_version函数
                 path1 = dict(zip(path, filenames))
-                path2 = dict(path1)
-                path2.update(path2)
+                path2.update(path1)
 
-    print(path2)
-
+        print(path2)
+        return (path2)
     try:
         new_path = os.path.join(maindir,subdir)
         test = get_all_files(new_path)
@@ -109,28 +81,68 @@ def get_all_files(folder_path):
 
 
 
-    # return apath,filename
+
 
 #test3 = get_all_files("C:\\Program Files (x86)\\COMODO\\test")
 
 def get_file_version1(folder_path):
     file_path = get_all_files(folder_path)
-    pe_names = file_path[1]
-    try:
-        info = win32api.GetFileVersionInfo(file_path[0], '\\')
-        ms = info['FileVersionMS']
-        ls = info['FileVersionLS']
-        version = '%d.%d.%d.%d' % (win32api.HIWORD(ms), win32api.LOWORD(ms), win32api.HIWORD(ls), win32api.LOWORD(ls))
+    i = 0
+    dict2 ={}
+    for key in file_path:
 
-        dict = {pe_names: version}
-    except:
-        print("the file no version")
+        pe_names = list(file_path.values())
+        try:
+            info = win32api.GetFileVersionInfo(key,'\\')
+            ms = info['FileVersionMS']
+            ls = info['FileVersionLS']
+            version = '%d.%d.%d.%d' % (win32api.HIWORD(ms), win32api.LOWORD(ms), win32api.HIWORD(ls), win32api.LOWORD(ls))
+
+            dict1 = {pe_names[i]: version}
+            dict2.update(dict1)
+            i=i+1
+        except:
+            print("the file no version:" + key)
 
 
-    print(dict)
+    print(dict2)
 
 #test1 = get_all_files("C:\\Program Files (x86)\\COMODO\\COMODO Secure Shopping")
-#test2 = get_file_version1("C:\\Program Files (x86)\\COMODO\\test")
-test3 = get_all_files("C:\\Program Files (x86)\\COMODO\\test")
+#test1 = get_file_version1("C:\\Program Files (x86)\\COMODO\\COMODO Secure Shopping")
+test2 = get_file_version1("C:\\Program Files (x86)\\COMODO\\test")
+#test3 = get_all_files("C:\\Program Files (x86)\\COMODO\\test")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#test
+def dict_test():
+    dict1 = {'1:1','2:2','3:3'}
+    dict2 = {'4:4','5:5','6:6'}
+    dict1.update(dict2)
+    print(dict1)
+
+
+#test4 = dict_test()
